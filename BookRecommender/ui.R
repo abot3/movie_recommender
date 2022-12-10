78a6ceee23fde3a17493b5c4d068f7f91d7166e9
aBotel <admbotel@uwaterloo.ca>
2022-12-10 03:20:17 -0800

    Baseline App UI working.

---
+++ BookRecommender/ui.R
@@ -0,0 +1,45 @@
+## ui.R
+library(shiny)
+library(shinydashboard)
+library(recommenderlab)
+library(data.table)
+library(ShinyRatingInput)
+library(shinyjs)
+
+source('functions/helpers.R')
+
+# https://campuswire.com/c/G3D46BBBA/feed/1267
+# https://campuswire.com/c/G3D46BBBA/feed/1269
+
+# shinyUI(
+ui = dashboardPage(
+          skin = "blue",
+          dashboardHeader(title = "Movie Recommender"),
+
+          dashboardSidebar(disable = TRUE),
+
+          dashboardBody(includeCSS("css/books.css"),
+              fluidRow(
+                  box(width = 12, title = "Step 1: Rate as many movies as possible",
+                      status = "info", solidHeader = TRUE, collapsible = TRUE,
+                      div(class = "rateitems",
+                          uiOutput('ratings')
+                      )
+                  )
+                ),
+              fluidRow(
+                  useShinyjs(),
+                  box(
+                    width = 12, status = "info", solidHeader = TRUE,
+                    title = "Step 2: Discover movies you might like",
+                    br(),
+                    withBusyIndicatorUI(
+                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                    ),
+                    br(),
+                    tableOutput("results")
+                  )
+               )
+          )
+    )
+# )
